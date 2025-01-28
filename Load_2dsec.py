1ad74cc948cc379e2baf12375bf06a911425a4e7
Andrés Mateo <andres.mateo@mi.unc.edu.ar>
2025-01-28 16:38:56 +0100

    load 2dsec avances
    Changed name from test.
    Load and saving folders system.
    Works with FLASH and RARE.
    Not MSME and some other errors.
    Resolution is by default with dpi=600

---
+++ Load_2dsec.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Testing how to import and plot 2dseq bruker files
+"""
+import brukerapi
+from brukerapi.dataset import Dataset
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# =============================================================================
+#                              Path selection
+# =============================================================================
+''' load folder '''
+dir_folder = 'H:/MRI/'
+dir_study = 'AM7T_250121_SPC_extrudate_1_1_20250121_093706'
+dir_experimet = '14'
+
+''' Save folder '''
+dir_save_folder = 'C:/Users/mateo006/Documents/Processed_data/28012025_Test/'.replace("\\", "/")
+dir_save_name = 'Testeesteste'
+save_path = Path(dir_save_folder + dir_save_name + '/' + dir_experimet)
+save_path.mkdir(parents=True, exist_ok=True)
+
+''' load data and make dataset'''
+dataset = Dataset( dir_folder + '/' + dir_study + '/' + dir_experimet + '/pdata/1/')
+sequence_name = dataset['VisuAcqSequenceName'].value
+data2d = dataset.data
+data2d.size
+
+# =============================================================================
+#                           Prepearing the data
+# =============================================================================
+
+# =============================================================================
+# print(dataset.TE)
+# print(dataset.TR)
+# print(dataset.imaging_frequency)
+# print(dataset.affine)
+# print(dataset['VisuAcqSequenceName'])
+# print(dataset._parameters['visu_pars']['VisuCoreExtent'].value)
+# =============================================================================
+
+''' Reading FOV '''
+fov = dataset._parameters['visu_pars']['VisuCoreExtent'].value
+npoints = dataset._parameters['visu_pars']['VisuCoreSize'].value
+resolution = fov[0]/npoints[0]
+print(resolution)
+''' with this we can make the axix '''
+axis_x = resolution * np.linspace(0, npoints[0], npoints[0])
+axis_y = resolution * np.linspace(0, npoints[1], npoints[1])
+
+
+
+# =============================================================================
+#                                Ploting 
+# =============================================================================
+
+
+for i in range(len(dataset.data[0,0,:])):
+    plt.contour(dataset.data[:,:,i], 
+                levels = 300, 
+                cmap='magma'
+                )
+    plt.title(sequence_name)
+    #plt.xticks(axis_x)
+    #plt.yticks(axis_y)
+    plt.xlabel('x [mm]')
+    plt.ylabel('y [mm]')
+    plt.colorbar()
+    plt.savefig(save_path / f"Test1_fig_{i+1}", dpi=600)
+    plt.show()
+    
+
+# =============================================================================
+#                           Usefull lines
+# =============================================================================
+'''
+print(dataset._parameters['visu_pars']['VisuCoreExtent'])  # Para ver qué tipo de objeto es
+print(dir(dataset))   # To see atributes and methods
+'''
+
